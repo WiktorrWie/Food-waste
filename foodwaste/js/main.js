2ca230028f8dc6e8f231a299b57c25ad11240a7d
WiktorrWie <eaawiwi@students.eaaa.dk>
2021-12-03 13:10:44 +0100

    first commit

---
+++ foodwaste/js/main.js
@@ -0,0 +1,16 @@
+function openSignUp() {
+    document.getElementById("signUp").style.display = "block";
+}
+  
+function closeSignUp() {
+    document.getElementById("signUp").style.display = "none";
+}
+  
+function openLogIn() {
+    document.getElementById("logIn").style.display = "block";
+}
+  
+function closeLogIn() {
+    document.getElementById("logIn").style.display = "none";
+}
+  
--- foodwaste/login/welcome.php
+++ foodwaste/login/welcome.php
@@ -2,8 +2,17 @@
 
 ?>
 
-<h2>Signup</h2>
+<link rel="stylesheet" href="../css/main.css" type="text/css">
+<script src="../js/main.js"></script>
 
+<div id="welcome">
+    <h5>Welcome to</h5>
+    
+
+</div>
+
+<h2 id="openSignUp" onclick="openSignUp()">Signup</h2>
+<div id="signUp">
 <form action="signup.php" method="POST">
     <input type="text" name="firstName" placeholder="First name">
     <input type="text" name="lastName" placeholder="Last name">
@@ -11,6 +20,8 @@
     <input type="password" name=password placeholder="password">
     <input type=submit>
 </form>
+<button type="button" id="closeSignUp" onclick="closeSignUp()">Close</button>
+</div>
 <?php 
 if (isset($_GET["signup"])){
     if ($_GET["signup"]=="error"){
@@ -23,14 +34,15 @@ if (isset($_GET["signup"])){
 ?>
 
 
-<h2>Log In</h2>
-
+<h2 id="openLogIn" onclick="openLogIn()">Log In</h2>
+<div id="logIn">
 <form action="login.php" method="POST">
     <input type="email" name=email placeholder="email">
     <input type="password" name=password placeholder="password">
     <input type=submit value=login>
 </form>
-
+<button type="button" id="closeLogIn" onclick="closeLogIn()">Close</button>
+</div>
 <?php 
 if (isset($_GET["signin"])){
     if ($_GET["signin"]=="error"){
