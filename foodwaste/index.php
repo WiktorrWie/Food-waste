3c0e309842d61f424c09e64ec9dab8f45fe3e217
WiktorrWie <eaawiwi@students.eaaa.dk>
2021-12-08 23:26:23 +0100

    Home page - 1st review

--- foodwaste/index.php
+++ foodwaste/index.php
@@ -7,16 +7,44 @@ if (empty($_SESSION)){
 }
 echo "<h3>Hey $_SESSION[first_name]</h3>";
 
-
-
-
-
 ?>
-<a href=addPost.php>Create post </a>
-<form action="logout.php" method="GET">
-<input name="logout "type=submit value="log-out">
-</form>
 
-<img src="images/posts/unga1638530774.png">
-<br>
-<a href="profile.php"> Profile </a>
+<link rel="stylesheet" href="../css/main.css" type="text/css">
+<script src="../js/main.js"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500&display=swap" rel="stylesheet">
+
+<body>
+<div class="header">
+            <a href="index.html" class="nav_link logo"><img src="./icons/MinMad.png" alt="logo" class="logo"></a>
+
+            <input class="menu-btn" type="checkbox" id="menu-btn">
+            <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+            <ul class="dropdown_menu">
+                <li class="menu_item"><a href="" class="nav_link">
+                        <h2 class="">Home</h2>
+                    </a></li>
+
+                <li class="menu_item"><a href="" class="nav_link">
+                        <h2 class="">Chat</h2>
+                    </a>
+                </li>
+
+                <li class="menu_item"><a href="profile.php" class="nav_link">
+                        <h2 class="">Profile</h2>
+                    </a>
+                </li>
+
+                <li class="menu_item"><a href="" class="nav_link">
+                    <form action="logout.php" method="GET">
+                        <input name="logout "type=submit value="log-out">
+                    </form>
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+        <a href=addPost.php>Create post </a>
+
+</body>
